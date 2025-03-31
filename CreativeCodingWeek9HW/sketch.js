828a4980a64781fe8ac62505ab828880162cfb92
Michael Oliver <mo134331@umconnect.umt.edu>
2025-03-31 12:02:33 -0600

    Finished J5 Assignment

---
+++ CreativeCodingWeek9HW/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+    rect(95,135,25,40);
+    rect(197,135,25,40);
+    circle(159,156,115);
+    triangle(150, 175, 158, 120, 168, 175);
+    circle(125,150,25);
+    circle(193,150,25);
+    point(125,150);
+    point(193,150);
+    line(135,190,183,190);
+    text('Michael Oliver',125, 250);
+  }
